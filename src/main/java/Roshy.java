fd48b8e34bbb6b217b8bfa774869a84e4efbaa9d
Roshan <rohanroshan2403@gmail.com>
2026-01-28 23:21:38 +0800

    Level -0

---
+++ src/main/java/Roshy.java
@@ -0,0 +1,10 @@
+public class Roshy {
+    public static void main(String[] args) {
+        String logo = " __  __            _       \n"
+                + "|  \\/  | __ _ _ __(_) ___  \n"
+                + "| |\\/| |/ _` | '__| |/ _ \\ \n"
+                + "| |  | | (_| | |  | | (_) |\n"
+                + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
+        System.out.println(logo + " Hello I'm Roshy\n What can I do for you?\n Bye. See you again soon!");
+    }
+}
