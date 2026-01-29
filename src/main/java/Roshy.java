78441c27c736fb25c75bbdb6faaea8598f6bd37b
Roshan <rohanroshan2403@gmail.com>
2026-01-29 23:16:42 +0800

    Add,List

--- src/main/java/Roshy.java
+++ src/main/java/Roshy.java
@@ -9,16 +9,37 @@ public class Roshy {
                 + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
         System.out.println(logo + " Hello I'm Roshy\n What can I do for you?\n");
         Scanner in = new Scanner(System.in);
+        String[] lister = new String[100];
+        String separator = "    ____________________________________________________________";
 
+        int index = 0;
         String line = "";
         while(!line.equals("bye")) {
+            //reading the next line
             line = in.nextLine();
+            //so that the value bye is not repeated
             if(line.equals("bye")){
                 break;
             }
-            System.out.println(line);
-            if(line.equals("Mario")){
-                System.out.println("Mario? MArioooooo LUIGIIIII MARIOOOO LUGIII MARIO" + logo + logo);
+            if(line.equals("list")){
+                if(index == 0) {
+                    System.out.println("No tasks yet!");
+                }
+                for(int i = 0; i < index; i++){
+                    System.out.println((i+1) + ": " + lister[i]);
+                }
+            }
+            else if(line.equals("Mario")){
+                System.out.println(separator +"\n" + "Mario? MArioooooo LUIGIIIII MARIOOOO LUGIII MARIO" + logo + logo);
+            }
+            else{
+                lister[index] = line;
+                index++;
+                System.out.println("added: " + line);
+            }
+
+            if(index == 100){
+                break;
             }
         }
         System.out.println("BYEEE ITS Super Smash BROS TIME");
