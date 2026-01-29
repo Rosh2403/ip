28043f3dbf16262f739bac0148fb7d3d71b00e00
Roshan <rohanroshan2403@gmail.com>
2026-01-29 20:35:49 +0800

    Echo

--- src/main/java/Roshy.java
+++ src/main/java/Roshy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Roshy {
     public static void main(String[] args) {
         String logo = " __  __            _       \n"
@@ -5,6 +7,21 @@ public class Roshy {
                 + "| |\\/| |/ _` | '__| |/ _ \\ \n"
                 + "| |  | | (_| | |  | | (_) |\n"
                 + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
-        System.out.println(logo + " Hello I'm Roshy\n What can I do for you?\n Bye. See you again soon!");
+        System.out.println(logo + " Hello I'm Roshy\n What can I do for you?\n");
+        Scanner in = new Scanner(System.in);
+
+        String line = "";
+        while(!line.equals("bye")) {
+            line = in.nextLine();
+            if(line.equals("bye")){
+                break;
+            }
+            System.out.println(line);
+            if(line.equals("Mario")){
+                System.out.println("Mario? MArioooooo LUIGIIIII MARIOOOO LUGIII MARIO" + logo + logo);
+            }
+        }
+        System.out.println("BYEEE ITS Super Smash BROS TIME");
+
     }
 }
