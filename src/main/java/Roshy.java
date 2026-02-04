38b60346639726118081c394da880df460685411
Roshan <rohanroshan2403@gmail.com>
2026-02-05 00:51:55 +0800

    To follow Coding Standards

--- src/main/java/Roshy.java
+++ src/main/java/Roshy.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class Roshy {
     public static void main(String[] args) {
-        String logo = " __  __            _       \n"
-                + "|  \\/  | __ _ _ __(_) ___  \n"
-                + "| |\\/| |/ _` | '__| |/ _ \\ \n"
-                + "| |  | | (_| | |  | | (_) |\n"
-                + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
+        String logo = " __  __            _       \n" + "|  \\/  | __ _ _ __(_) ___  \n" + "| |\\/| |/ _` | '__| |/ _ \\ \n" + "| |  | | (_| | |  | | (_) |\n" + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
         System.out.println(logo + " Hello I'm Roshy\n What can I do for you?\n");
         Scanner in = new Scanner(System.in);
         Task[] lister = new Task[100];
@@ -14,39 +10,35 @@ public class Roshy {
 
         int index = 0;
         String line = "";
-        while(!line.equals("bye")) {
+        while (!line.equals("bye")) {
             //reading the next line
             line = in.nextLine();
             //so that the value bye is not repeated
-            if(line.equals("bye")){
+            if (line.equals("bye")) {
                 break;
             }
-            if(line.equals("list")){
-                if(index == 0) {
+            if (line.equals("list")) {
+                if (index == 0) {
                     System.out.println("No tasks yet!");
-                }
-                else {
+                } else {
                     System.out.println("Here are the tasks in your list\n");
                     for (int i = 0; i < index; i++) {
                         System.out.println((i + 1) + ": " + "[" + lister[i].getStatusIcon() + "] " + lister[i].description);
                     }
                 }
-            }
-            else if(line.equals("Mario")){
-                System.out.println(separator +"\n" + "Mario? MArioooooo LUIGIIIII MARIOOOO LUGIII MARIO" + logo + logo);
-            }
-            else if(line.startsWith("mark")){
+            } else if (line.equals("Mario")) {
+                System.out.println(separator + "\n" + "Mario? MArioooooo LUIGIIIII MARIOOOO LUGIII MARIO" + logo + logo);
+            } else if (line.startsWith("mark")) {
                 int taskNum = Integer.parseInt(line.substring(5)) - 1;
                 lister[taskNum].markAsDone();
-                System.out.println("Nice Ive marked this as done!\n" + "[" + lister[taskNum].getStatusIcon() +"] " + lister[taskNum].description);
-            }
-            else{
+                System.out.println("Nice Ive marked this as done!\n" + "[" + lister[taskNum].getStatusIcon() + "] " + lister[taskNum].description);
+            } else {
                 lister[index] = new Task(line);
                 index++;
                 System.out.println("added: " + line);
             }
 
-            if(index == 100){
+            if (index == 100) {
                 break;
             }
         }
