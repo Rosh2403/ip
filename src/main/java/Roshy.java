8399937d12fb43257526010470a1e741524376c4
Roshan <rohanroshan2403@gmail.com>
2026-03-08 01:01:13 +0800

    Add save and load functionality - Level 7

--- src/main/java/Roshy.java
+++ src/main/java/Roshy.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Roshy {
     private static final String LOGO = "_____\n" + "|  \\/  | __ _ _ __(_) ___  \n" + "| |\\/| |/ _` | '__| |/ _ \\ \n" + "| |  | | (_| | |  | | (_) |\n" + "|_|  |_|\\__,_|_|  |_|\\___/ \n";
@@ -22,7 +25,8 @@ public class Roshy {
         roshy.run();
     }
 
-    public void run() {
+    public void run() throws IOException {
+        loadTasks();
         System.out.println(LOGO + " Hello I'm Roshy\n What can I do for you?\n");
         while (true) {
             String line = scanner.nextLine();
@@ -68,25 +72,27 @@ public class Roshy {
         }
     }
 
-    private void handleMark(String line) {
+    private void handleMark(String line) throws IOException{
         if (hasNoDescription(line, "mark")) {
             return;
         }
         int taskNum = Integer.parseInt(line.substring(MARK_PREFIX_LENGTH)) - 1;
         tasks[taskNum].markAsDone();
         System.out.println("Nice I've marked this as done!\n" + tasks[taskNum].toString());
+        saveTasks();
     }
 
-    private void handleTodo(String line) {
+    private void handleTodo(String line) throws IOException {
         if (hasNoDescription(line, "todo")) {
             return;
         }
         tasks[taskCount] = new Todo(line.substring(TODO_PREFIX_LENGTH));
         taskCount++;
         System.out.println(tasks[taskCount - 1].toString());
+        saveTasks();
     }
 
-    private void handleDeadline(String line) {
+    private void handleDeadline(String line) throws IOException {
         if (hasNoDescription(line, "deadline")) {
             return;
         }
@@ -96,9 +102,10 @@ public class Roshy {
         tasks[taskCount] = new Deadline(description, byDate);
         taskCount++;
         System.out.println(tasks[taskCount - 1].toString());
+        saveTasks();
     }
 
-    private void handleEvent(String line) {
+    private void handleEvent(String line) throws IOException {
         if (hasNoDescription(line, "event")) {
             return;
         }
@@ -110,6 +117,7 @@ public class Roshy {
         tasks[taskCount] = new Event(description, toWhen, fromWhen);
         taskCount++;
         System.out.println(tasks[taskCount - 1].toString());
+        saveTasks();
     }
 
     private boolean hasNoDescription(String line, String command) {
@@ -118,5 +126,49 @@ public class Roshy {
             return true;
         }
         return false;
+    }
+    private void saveTasks() throws IOException{
+        String text = "";
+        for(int i = 0; i < taskCount; i++){
+            if(tasks[i] instanceof Todo){
+                text += "T | " + ((tasks[i].isDone ? "1" : "0") + " | " + tasks[i].description + "\n");
+            } else if (tasks[i] instanceof Deadline) {
+                Deadline d = (Deadline) tasks[i];
+                text += "D | " + ((tasks[i].isDone ? "1" : "0") + " | " + tasks[i].description + " | " + d.getByDate() + "\n");
+            } else if (tasks[i] instanceof Event){
+                Event e = (Event) tasks[i];
+                text += "E | " + ((tasks[i].isDone ? "1" : "0") + " | " + tasks[i].description + " | " + e.getFromWhen() + " | " + e.getToWhen() + "\n");
+            }
+        }
+        writeToFile("./data/roshy.txt", text);
+    }
+    private void writeToFile(String filePath, String text) throws IOException {
+        new File(filePath).getParentFile().mkdirs();
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(text);
+        fw.close();
+    }
+    private void loadTasks() throws IOException {
+        File file = new File("./data/roshy.txt");
+        if (!file.exists()) {
+            return;
+        }
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            String[] parts = line.split(" \\| ");
+            if (parts[0].equals("T")) {
+                tasks[taskCount] = new Todo(parts[2]);
+            } else if (parts[0].equals("D")) {
+                tasks[taskCount] = new Deadline(parts[2], parts[3]);
+            } else if (parts[0].equals("E")) {
+                tasks[taskCount] = new Event(parts[2], parts[3], parts[4]);
+            }
+            if (parts[1].equals("1")) {
+                tasks[taskCount].markAsDone();
+            }
+            taskCount++;
+        }
+        fileScanner.close();
     }
 }
